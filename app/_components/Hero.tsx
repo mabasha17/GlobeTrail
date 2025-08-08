f20834868271bc159b73ca09759a441a3c570b9b
mabasha17 <mabaasha.shaik@gmail.com>
2025-08-08 15:58:07 +0530

    updated

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Globe2, Landmark, Plane, Send } from "lucide-react";
 import HeroVideoDialog from "@/components/magicui/hero-video-dialog";
 
-const suggestions = [
+const TravelIdeas = [
   {
     title: "Create a trip for Paris from India",
     icon: <Globe2 className="text-blue-400 h-5 w-5" />,
@@ -25,12 +25,12 @@ const suggestions = [
 
 function Hero() {
   return (
-    <div className="mt-24 w-full flex justify-center">
-      <div className="mx-w-3xl w-full text-center space-y-6">
-        <h1 className="text-xl md:text-5xl font-bold ">
+    <div className="mt-24 w-full flex justify-center ">
+      <div className="max-w-3xl w-full text-center space-y-6">
+        <h1 className="text-3xl md:text-5xl font-bold ">
           Plan Your Next Adventure
         </h1>
-        <p className="text-lg">
+        <p className="text-lg text-muted-foreground">
           Discover the world with personalized travel plans powered by AI.
         </p>
         {/* input box */}
@@ -40,32 +40,32 @@ function Hero() {
               placeholder="create a trip for Paris from India..."
               className="w-full h-28 bg-transparent border-none focus-visible:ring-0"
             />
-            <Button size={"icon"} className="absolute bottom-6 right-6">
+            <Button size={"icon"} className="absolute bottom-6 right-6 hover:scale-105 transition-all duration-300">
               <Send className="h-4 w-4" />
             </Button>
           </div>
         </div>
-        {/* suggestion list */}
-        <div className=" flex justify-center mx-w-xl mx-auto ">
-          {suggestions.map((suggestion, index) => (
+        {/* suggestions list */}
+        <div className=" flex flex-wrap justify-center gap-3 max-w-xl mx-auto ">
+          {TravelIdeas.map((s,i) => (
             <div
-              key={index}
+              key={i}
               className="flex items-center gap-2 border rounded-full
               cursor-pointer p-2 hover:bg-primary"
             >
-              {suggestion.icon}
-              <span className="text-sm">{suggestion.title}</span>
+              {s.icon}
+              <span className="text-sm">{s.title}</span>
             </div>
           ))}
         </div>
         {/* video section */}
-        <HeroVideoDialog
+        {/* <HeroVideoDialog
           className="hidden dark:block"
           animationStyle="from-center"
           videoSrc=""
           thumbnailSrc=""
           thumbnailAlt="Hero Video"
-        />
+        /> */}
       </div>
     </div>
   );
